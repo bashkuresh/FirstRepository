52f0d9ccb5ade8157787c808fadb4449eb48b7bd
Arthur <bashkuresh1992@gmail.com>
2018-12-13 17:13:09 -0500

    ArrayTas1

--- src/Random.java
+++
@@ -1,9 +0,0 @@
-public class Random {
-    public static void main(String[] args) {
-        java.util.Random random = new java.util.Random();
-        boolean b =random.nextBoolean() ;
-        if (b == true ){
-            System.out.println("Rent a car!");
-        } else System.out.println("Bus!");
-    }
-}
