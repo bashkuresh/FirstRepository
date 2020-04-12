ff0ccb99396483f23af84ed48f6d0f7222dfa57d
arthursmith <bashkuresh1992@gmail.com>
2020-04-12 11:46:03 -0400

    Branching

---
+++ src/Branching.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Branching {
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Inter Length");
+            int a = scanner.nextInt();
+            int b = a;
+
+            for (int i = 1; i <= a; i++){
+                for (int j = i; j > 0; j--){
+                    System.out.print(i + " ");
+                    b--;
+                    if (b == 0){
+                        break;
+                    }
+                }
+                if (b == 0){
+                    break;
+                }
+            }
+        }
+    }
+
